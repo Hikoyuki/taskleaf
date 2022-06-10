ce2db9c37c0a47352e62be47a4c7c121ccf33c31
酒井彦行 <hikoyuki.sakai@gmail.com>
2022-06-10 21:08:56 +0900

    Fi task4-4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,9 +12,19 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    # @インスタンス変数なのは、検証エラー時にviewに検証を行った現物のTaskオブジェクトを渡す必要があるため
+    # 1.フォーム送信したデータが入ってるため、前回操作した値をフォームに引き継げる ユーザーが再入力しなくて済む
+    # 2.Taskオブジェクトの抱える検証エラーの内容をユーザーに対して表示することができる
+    @task = Task.new(task_params)
+
+    # 検証を追加したのでsave!ではなく、saveで戻り値によって制御を変える
+    if @task.save
+      redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+   else
+    # 検証結果がfalseなら、登録フォームを表示して再入力を促す
+    render :new
+   end
+
     # redirect_toのオプションに直接渡せるFlashキーは、:noticeと:alertのみ
     # warningのキーを渡す場合 redirect_to task_url, flash: {warning: "何かの警告メッセージ"}
     # メソッドの形で渡す場合　　flash.notice = "タスク「#{task.name}」を登録しました。"
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_03_153306) do
+ActiveRecord::Schema.define(version: 2022_06_08_150720) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
