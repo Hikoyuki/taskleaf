5992aa41efa8e7d663d62bc74cc34f8475d12535
酒井彦行 <hikoyuki.sakai@gmail.com>
2022-06-17 00:19:18 +0900

    add User

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # 1行追加で has_secure_passwordが使える
+  # これによりデータベースのカラムにない2つの属性が追加される
+  # password属性 入力されたパスワードを一時的に格納するための属性 メモリ上に一時的
+  # password_confirmation パスワード登録時に確認用のパスワードを入力用 同様に一時的にUserクラスで格納
+  # 2つが一致するか検証が走る 一致する場合にpassword_digestが生成される
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # r g controller Admin::Users new edit .. コントローラのモジュールの名前空間
+  namespace :admin do # namespasce :adminの中に定義してるため、URLに/admin、ヘルパーメソッドにadmin_ がつくようになる
+    resources :users
+  end
+
   root to: 'tasks#index'
   resources :tasks
 end
